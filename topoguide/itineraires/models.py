12db90485447ffbfca2c3d9457447538bee3705f
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-17 18:17:59 +0200

    Création de la table et configuration de l'application

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
+class Itineraire(models.Model):
+    titre = models.CharField(max_length=200)
+    depart = models.CharField(max_length=200)
+    description = models.TextField()
+    altitude_depart = models.IntegerField("Altitude (en m)")
+    altitude_min = models.IntegerField("Altitude minimum (en m)")
+    altitude_max = models.IntegerField("Altitude maximum (en m)")
+    denivele_pos = models.IntegerField("Dénivelé positif cumulé (en m")
+    denivele_neg = models.IntegerField("Dénivelé négatif cumulé (en m")
+    duree_estimee = models.FloatField("Durée estimée (en h)")
+    difficulte_estimee = models.IntegerField("Difficulté estimée (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    
+
+class Sortie(models.Model):
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+    itineraire = models.ForeignKey('Itineraire', on_delete=models.CASCADE)
+    date_sortie = models.DateField("Date de sortie")
+    duree_reelle = models.FloatField("Durée réelle (en h)")
+    nb_personne = models.IntegerField("Nombre de personnes ayant participé à la sortie")
+    
+    ## attribut expérience du groupe
+
+    NIVEAUX = [('0', ('Tous débutants')),
+       ('1', ('Mixte')),
+       ('2', ('Tous experts')),]
+    niveaux = models.CharField("Niveau des personnes", max_length=32, choices=NIVEAUX)
+
+    
+    METEO = [('0', ('Bonne météo')),
+       ('1', ('Météo moyenne')),
+       ('2', ('Mauvaise météo')),]
+    meteo = models.CharField("Météo", max_length=32, choices=METEO)
+    difficulte_reelle = models.IntegerField("Difficulté ressentie (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+
