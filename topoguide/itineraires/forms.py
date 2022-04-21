a9ef94a47e58fae3e504a92d5b9da42edc185d80
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-21 09:53:25 +0200

    Modification du formulaire pour préremplir l'itinéraire

--- topoguide/itineraires/forms.py
+++ topoguide/itineraires/forms.py
@@ -6,4 +6,4 @@ class SortieForm(forms.ModelForm):
 
     class Meta:
         model = Sortie
-        fields = ('itineraire', 'duree_reelle','duree_reelle','nb_personne','niveaux','meteo','difficulte_reelle','date_de_sortie')
+        fields = ('duree_reelle','duree_reelle','nb_personne','niveaux','meteo','difficulte_reelle','date_de_sortie')
