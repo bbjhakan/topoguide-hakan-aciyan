62eb4e605908428346bf8dfbfe46ab93abf8a9f3
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-21 23:52:06 +0200

    Ajout de conditions pour valider les champs (entier positifs, ..)

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -12,13 +12,13 @@ class Itineraire(models.Model):
     titre = models.CharField(max_length=200)
     depart = models.CharField(max_length=200)
     description = models.TextField() #texte de description du parcours
-    altitude_depart = models.IntegerField("Altitude (en m)")
-    altitude_min = models.IntegerField("Altitude minimum (en m)")
-    altitude_max = models.IntegerField("Altitude maximum (en m)")
-    denivele_pos = models.IntegerField("Dénivelé positif cumulé (en m")
-    denivele_neg = models.IntegerField("Dénivelé négatif cumulé (en m")
-    duree_estimee = models.FloatField("Durée estimée (en h)")
-    difficulte_estimee = models.IntegerField("Difficulté estimée (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    altitude_depart = models.PositiveIntegerField("Altitude (en m)")
+    altitude_min = models.PositiveIntegerField("Altitude minimum (en m)")
+    altitude_max = models.PositiveIntegerField("Altitude maximum (en m)")
+    denivele_pos = models.PositiveIntegerField("Dénivelé positif cumulé (en m")
+    denivele_neg = models.PositiveIntegerField("Dénivelé négatif cumulé (en m")
+    duree_estimee = models.FloatField("Durée estimée (en h)", validators=[MinValueValidator(0)])
+    difficulte_estimee = models.PositiveIntegerField("Difficulté estimée (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     
     def __str__(self):
         return self.titre
@@ -27,8 +27,8 @@ class Itineraire(models.Model):
 class Sortie(models.Model):
    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
    itineraire = models.ForeignKey('Itineraire', on_delete=models.CASCADE)
-   duree_reelle = models.FloatField("Durée réelle (en h)")
-   nb_personne = models.IntegerField("Nombre de personnes ayant participé à la sortie")
+   duree_reelle = models.FloatField("Durée réelle (en h)",validators=[MinValueValidator(0)])
+   nb_personne = models.PositiveIntegerField("Nombre de personnes ayant participé à la sortie")
    
    ## attribut expérience du groupe
    NIVEAUX = [(CHOICE1, ('Tous débutants')),
