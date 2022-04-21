b4fe8d9b46bc24cdd67a54de23d54ade93e5e1db
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-22 00:36:01 +0200

    Amélioration de la lisibilité du code

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -9,19 +9,19 @@ CHOICE3 = '2'
 
 # Create your models here.
 class Itineraire(models.Model):
-    titre = models.CharField(max_length=200)
-    depart = models.CharField(max_length=200)
-    description = models.TextField() #texte de description du parcours
-    altitude_depart = models.PositiveIntegerField("Altitude (en m)")
-    altitude_min = models.PositiveIntegerField("Altitude minimum (en m)")
-    altitude_max = models.PositiveIntegerField("Altitude maximum (en m)")
-    denivele_pos = models.PositiveIntegerField("Dénivelé positif cumulé (en m")
-    denivele_neg = models.PositiveIntegerField("Dénivelé négatif cumulé (en m")
-    duree_estimee = models.FloatField("Durée estimée (en h)", validators=[MinValueValidator(0)])
-    difficulte_estimee = models.PositiveIntegerField("Difficulté estimée (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+   titre = models.CharField(max_length=200)
+   depart = models.CharField(max_length=200)
+   description = models.TextField() #texte de description du parcours
+   altitude_depart = models.PositiveIntegerField("Altitude (en m)")
+   altitude_min = models.PositiveIntegerField("Altitude minimum (en m)")
+   altitude_max = models.PositiveIntegerField("Altitude maximum (en m)")
+   denivele_pos = models.PositiveIntegerField("Dénivelé positif cumulé (en m")
+   denivele_neg = models.PositiveIntegerField("Dénivelé négatif cumulé (en m")
+   duree_estimee = models.FloatField("Durée estimée (en h)", validators=[MinValueValidator(0)])
+   difficulte_estimee = models.PositiveIntegerField("Difficulté estimée (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     
-    def __str__(self):
-        return self.titre
+   def __str__(self):
+      return self.titre
      
      
 class Sortie(models.Model):
@@ -41,6 +41,7 @@ class Sortie(models.Model):
       (CHOICE2, ('Météo moyenne')),
       (CHOICE3, ('Mauvaise météo')),]
    meteo = models.CharField("Météo", max_length=32, choices=METEO)
+   
    difficulte_reelle  = models.IntegerField("Difficulté ressentie (de 1 à 5)",default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
    date_de_sortie = models.DateField("Date de sortie")
    
--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -4,21 +4,25 @@ from .forms import SortieForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+#Vue avec une liste des itinéraires
 def itineraires(request):
     itineraires = get_list_or_404(Itineraire)
+    
     return render(request, 'itineraires/itineraires.html', {'itineraires': itineraires})
+
 
 #Vue pour les détails sur un itinéraire
 def itineraire_details(request, itineraire_id):
     it = get_object_or_404(Itineraire, pk=itineraire_id)
     
-    #J'ajoute les variable h et mn pour pouvoir afficher l'heure d'une manière plus jolie car mon attribut duree_estimee est un float
+    #J'ajoute les variable h et mn (et les ajoute dans le contexte) pour pouvoir afficher l'heure d'une manière plus jolie car mon attribut duree_estimee est un float
     h = int(it.duree_estimee) 
     mn = int((it.duree_estimee-h)*60)
+    
     sorties = Sortie.objects.filter(itineraire_id=itineraire_id)
     
     return render(request, 'itineraires/itineraire_details.html',{'it':it, 'sorties':sorties,'heure':h, 'minute':mn})
+
 
 #Vue pour les détails sur une sortie
 def sortie(request, sortie_id):
@@ -32,13 +36,16 @@ def sortie(request, sortie_id):
     ##Les deux variables qui suivent sont pour comparer avec les données estimées
     h_it = int(sortie.itineraire.duree_estimee) 
     mn_it = int((sortie.itineraire.duree_estimee-h_it)*60)
-    #Ajout dans le context de s pour pouvoir faire une barre de navigation lisible (voir sortie.html)
+    
+    #Ajout dans le contexte de s (qui donne "nom de la sortie - par utilisateur") pour pouvoir faire une barre de navigation lisible (voir sortie.html)
     return render(request, 'itineraires/sortie.html', {'sortie':sortie, 'attributs':attributs, 's': str(sortie),'heure':h, 'minute':mn,'hit':h_it, 'mnit': mn_it}) 
+
 
 #Vue pour créer une nouvelle sortie avec un formulaire
 @login_required
 def nouvelle_sortie(request, itineraire_id):
     itineraire = get_object_or_404(Itineraire, pk=itineraire_id)
+    
     if request.method == "POST":
         form = SortieForm(request.POST)
         if form.is_valid():
@@ -52,6 +59,7 @@ def nouvelle_sortie(request, itineraire_id):
         form = SortieForm()
         
     return render(request, 'itineraires/nouvelle_sortie.html', {'form': form, 'itineraire': itineraire})
+
 
 #Vue pour modifier une sortie avec un formulaire pré-rempli  
 @login_required
@@ -68,5 +76,5 @@ def modifier_sortie(request, sortie_id):
     else:
         form = SortieForm(instance=sortie)
         
-    #Ajout dans le context de s pour pouvoir faire une barre de navigation lisible (voir sortie_edit.html)
+    #Ajout dans le contexte de s (qui donne "nom de la sortie - par utilisateur") pour pouvoir faire une barre de navigation lisible (voir sortie_edit.html)
     return render(request, 'itineraires/sortie_edit.html', {'form': form, 'sortie':sortie, 's': str(sortie)})
