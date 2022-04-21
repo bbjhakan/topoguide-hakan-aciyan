bed868bdf83e23ce684ef6e2c749bbc08a106e32
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-22 00:30:55 +0200

    Amélioration du visuel des pages (afficher des heures et mn à la place de flottant) et comparaison donnée réelle/estimée

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -7,23 +7,33 @@ from django.contrib.auth.decorators import login_required
 
 def itineraires(request):
     itineraires = get_list_or_404(Itineraire)
-    
     return render(request, 'itineraires/itineraires.html', {'itineraires': itineraires})
 
 #Vue pour les détails sur un itinéraire
 def itineraire_details(request, itineraire_id):
     it = get_object_or_404(Itineraire, pk=itineraire_id)
+    
+    #J'ajoute les variable h et mn pour pouvoir afficher l'heure d'une manière plus jolie car mon attribut duree_estimee est un float
+    h = int(it.duree_estimee) 
+    mn = int((it.duree_estimee-h)*60)
     sorties = Sortie.objects.filter(itineraire_id=itineraire_id)
     
-    return render(request, 'itineraires/itineraire_details.html',{'it':it, 'sorties':sorties})
+    return render(request, 'itineraires/itineraire_details.html',{'it':it, 'sorties':sorties,'heure':h, 'minute':mn})
 
 #Vue pour les détails sur une sortie
 def sortie(request, sortie_id):
     sortie = get_object_or_404(Sortie, pk=sortie_id)
     attributs = Sortie.objects.filter(id=sortie_id)
     
+    #J'ajoute les variable h et mn pour pouvoir afficher l'heure d'une manière plus jolie car mon attribut duree_estimee est un float (bien évidemment, l'itinéraire dure au moins 1 heure)
+    h = int(sortie.duree_reelle) 
+    mn = int((sortie.duree_reelle-h)*60)
+    
+    ##Les deux variables qui suivent sont pour comparer avec les données estimées
+    h_it = int(sortie.itineraire.duree_estimee) 
+    mn_it = int((sortie.itineraire.duree_estimee-h_it)*60)
     #Ajout dans le context de s pour pouvoir faire une barre de navigation lisible (voir sortie.html)
-    return render(request, 'itineraires/sortie.html', {'sortie':sortie, 'attributs':attributs, 's': str(sortie)}) 
+    return render(request, 'itineraires/sortie.html', {'sortie':sortie, 'attributs':attributs, 's': str(sortie),'heure':h, 'minute':mn,'hit':h_it, 'mnit': mn_it}) 
 
 #Vue pour créer une nouvelle sortie avec un formulaire
 @login_required
