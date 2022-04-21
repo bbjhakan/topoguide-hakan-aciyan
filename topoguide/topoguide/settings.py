77690d51771a194b42b62647d93ba0975e43ca8e
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-04-21 23:45:43 +0200

    Suppression des fichiers statiques et ajout de bootstrap

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
